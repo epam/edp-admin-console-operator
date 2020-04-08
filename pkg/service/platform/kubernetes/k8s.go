epmd-edp/admin-console-operator

[EPMDEDP-4962] add test report framework handling
Change-Id: Ic49869b5b4509d997ae0ecefade58f9396874703

pkg/service/platform/kubernetes/k8s.go
@@ -162,6 +162,10 @@ func (service K8SService) CreateDeployConf(ac v1alpha1.AdminConsole, url string)
 									Value: "maven",
 								},
 								{
+									Name:  "TEST_REPORT",
+									Value: ac.Spec.EdpSpec.TestReport,
+								},
+								{
 									Name:  "DEPLOYMENT_SCRIPT",
 									Value: "helm-chart,openshift-template",
 								},

pkg/service/platform/openshift/openshift.go
@@ -176,6 +176,10 @@ func (service OpenshiftService) CreateDeployConf(ac v1alpha1.AdminConsole, url s
 									Value: "maven",
 								},
 								{
+									Name:  "TEST_REPORT",
+									Value: ac.Spec.EdpSpec.TestReport,
+								},
+								{
 									Name:  "DEPLOYMENT_SCRIPT",
 									Value: "helm-chart,openshift-template",
 								},

epam/edp-admin-console-operator

Change-Id: Ib353dccb675d72b78468c58b4782b34170b0ec6f

pkg/service/platform/kubernetes/k8s.go
@@ -106,7 +106,7 @@ func (service K8SService) CreateDeployConf(ac v1alpha1.AdminConsole, url string)
 									Value: db,
 								},
 								{
-									Name:  "EDP_VERSION",
+									Name: "EDP_VERSION",
 									ValueFrom: &coreV1Api.EnvVarSource{
 										ConfigMapKeyRef: &coreV1Api.ConfigMapKeySelector{
 											LocalObjectReference: coreV1Api.LocalObjectReference{
@@ -160,6 +160,10 @@ func (service K8SService) CreateDeployConf(ac v1alpha1.AdminConsole, url string)
 								{
 									Name:  "BUILD_TOOLS",
 									Value: "maven",
+								},
+								{
+									Name:  "TEST_REPORT_TOOLS",
+									Value: ac.Spec.EdpSpec.TestReportTools,
 								},
 								{
 									Name:  "DEPLOYMENT_SCRIPT",
@@ -699,7 +703,7 @@ func (service K8SService) CreateExternalEndpoint(ac v1alpha1.AdminConsole) error
 
 	so, err := service.CoreClient.Services(ac.Namespace).Get(ac.Name, metav1.GetOptions{})
 	if err != nil {
-		log.Info("Console Service has not been found","Namespace", ac.Namespace, "Name", ac.Name)
+		log.Info("Console Service has not been found", "Namespace", ac.Namespace, "Name", ac.Name)
 		return err
 	}
 
@@ -756,7 +760,7 @@ func (service K8SService) CreateExternalEndpoint(ac v1alpha1.AdminConsole) error
 func (service K8SService) GetConfigmapData(namespace string, name string) (map[string]string, error) {
 	c, err := service.CoreClient.ConfigMaps(namespace).Get(name, metav1.GetOptions{})
 	if err != nil {
-		if k8serrors.IsNotFound(err)  {
+		if k8serrors.IsNotFound(err) {
 			log.Info("Config Map not found", "Namespace", namespace, "Name", name)
 			return map[string]string{}, nil
 		}

pkg/service/platform/openshift/openshift.go
@@ -41,7 +41,7 @@ type OpenshiftService struct {
 	kubernetes.K8SService
 
 	authClient     authV1Client.AuthorizationV1Client
-	templateClient templateV1Client.TemplateV1Client 
+	templateClient templateV1Client.TemplateV1Client
 	projectClient  projectV1Client.ProjectV1Client
 	securityClient securityV1Client.SecurityV1Client
 	appClient      appsV1client.AppsV1Client
@@ -116,7 +116,7 @@ func (service OpenshiftService) CreateDeployConf(ac v1alpha1.AdminConsole, url s
 									Value: dbEnabled,
 								},
 								{
-									Name:  "EDP_VERSION",
+									Name: "EDP_VERSION",
 									ValueFrom: &coreV1Api.EnvVarSource{
 										ConfigMapKeyRef: &coreV1Api.ConfigMapKeySelector{
 											LocalObjectReference: coreV1Api.LocalObjectReference{
@@ -131,7 +131,7 @@ func (service OpenshiftService) CreateDeployConf(ac v1alpha1.AdminConsole, url s
 									Value: keycloakEnabled,
 								},
 								{
-									Name:  "DNS_WILDCARD",
+									Name: "DNS_WILDCARD",
 									ValueFrom: &coreV1Api.EnvVarSource{
 										ConfigMapKeyRef: &coreV1Api.ConfigMapKeySelector{
 											LocalObjectReference: coreV1Api.LocalObjectReference{
@@ -174,6 +174,10 @@ func (service OpenshiftService) CreateDeployConf(ac v1alpha1.AdminConsole, url s
 								{
 									Name:  "BUILD_TOOLS",
 									Value: "maven",
+								},
+								{
+									Name:  "TEST_REPORT_TOOLS",
+									Value: ac.Spec.EdpSpec.TestReportTools,
 								},
 								{
 									Name:  "DEPLOYMENT_SCRIPT",