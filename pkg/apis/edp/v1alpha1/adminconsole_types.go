epmd-edp/admin-console-operator

[EPMDEDP-4962] add test report framework handling
Change-Id: Ic49869b5b4509d997ae0ecefade58f9396874703

pkg/apis/edp/v1alpha1/adminconsole_types.go
@@ -26,6 +26,7 @@ type EdpSpec struct {
 	Name                  string `json:"name, omitempty"`
 	DnsWildcard           string `json:"dnsWildcard"`
 	IntegrationStrategies string `json:"integrationStrategies,omitempty"`
+	TestReport            string `json:"testReport,omitempty"`
 }
 
 type KeycloakSpec struct {

pkg/service/platform/kubernetes/k8s.go
@@ -162,6 +162,10 @@ func (service K8SService) CreateDeployConf(ac v1alpha1.AdminConsole, url string)
 									Value: "maven",
 								},
 								{
+									Name:  "TEST_REPORT",
+									Value: ac.Spec.EdpSpec.TestReport,
+								},
+								{
 									Name:  "DEPLOYMENT_SCRIPT",
 									Value: "helm-chart,openshift-template",
 								},

pkg/service/platform/openshift/openshift.go
@@ -176,6 +176,10 @@ func (service OpenshiftService) CreateDeployConf(ac v1alpha1.AdminConsole, url s
 									Value: "maven",
 								},
 								{
+									Name:  "TEST_REPORT",
+									Value: ac.Spec.EdpSpec.TestReport,
+								},
+								{
 									Name:  "DEPLOYMENT_SCRIPT",
 									Value: "helm-chart,openshift-template",
 								},

epam/edp-admin-console-operator

Change-Id: I36e9d1b81839a8eae04571e59f3c4d0fd484faa9

pkg/apis/edp/v1alpha1/adminconsole_types.go
@@ -26,6 +26,7 @@ type EdpSpec struct {
 	Name                  string `json:"name, omitempty"`
 	DnsWildcard           string `json:"dnsWildcard"`
 	IntegrationStrategies string `json:"integrationStrategies,omitempty"`
+	TestReportTools       string `json:"testReportTools,omitempty"`
 }
 
 type KeycloakSpec struct {

pkg/service/platform/kubernetes/k8s.go
@@ -162,6 +162,10 @@ func (service K8SService) CreateDeployConf(ac v1alpha1.AdminConsole, url string)
 									Value: "maven",
 								},
 								{
+									Name:  "TEST_REPORT_TOOLS",
+									Value: ac.Spec.EdpSpec.TestReportTools,
+								},
+								{
 									Name:  "DEPLOYMENT_SCRIPT",
 									Value: "helm-chart,openshift-template",
 								},

pkg/service/platform/openshift/openshift.go
@@ -176,6 +176,10 @@ func (service OpenshiftService) CreateDeployConf(ac v1alpha1.AdminConsole, url s
 									Value: "maven",
 								},
 								{
+									Name:  "TEST_REPORT_TOOLS",
+									Value: ac.Spec.EdpSpec.TestReportTools,
+								},
+								{
 									Name:  "DEPLOYMENT_SCRIPT",
 									Value: "helm-chart,openshift-template",
 								},