b71712513a1822ebae2ea68587d9e6b0001db74e
Anton_Tarianyk <Anton_Tarianyk@epam.com>
2021-08-04 12:29:20 +0300

    [EPMDEDP-6825]: Refactor hack/e2e/clean.sh script
    Change-Id: If49c1f34762ce66cd27c480d9c15af14af134b50

--- hack/e2e/clean.sh
+++ hack/e2e/clean.sh
@@ -1,15 +1,8 @@
-RELEASE_NAME="admin-console-operator"
-
 # 1 arg - namespace to deploy testable data
 clean() {
-	local ns=$( kubectl get ns "$1" -ojson | jq -r  '.metadata.name' )
+	local ns=$( kubectl get ns "$1" -o=jsonpath='{.metadata.name}' )
 	if [ ! ${ns} ]; then
 		return 0
-	fi
-	
-	local content=$( helm -n $1 status "${RELEASE_NAME}" -o json | jq -r  '.info.status' ) 
-	if [ ${content} ]; then
-		helm -n $1 delete "${RELEASE_NAME}"
 	fi
 	
 	kubectl delete ns $1
