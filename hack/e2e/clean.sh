2fbd7d9d8c356bbc1acaf3d067b4943720438766
Anton_Tarianyk <Anton_Tarianyk@epam.com>
2021-08-26 12:04:16 +0300

    [EPMDEDP-7089]: Modify clean script
    * ignore not found error during getting ns;
    
    Change-Id: I9df8a476ce927f8c1c2ee94368e6d826a270fd01

--- hack/e2e/clean.sh
+++ hack/e2e/clean.sh
@@ -1,6 +1,6 @@
 # 1 arg - namespace to deploy testable data
 clean() {
-	local ns=$( kubectl get ns "$1" -o=jsonpath='{.metadata.name}' )
+	local ns=$( kubectl get ns "$1" -o=jsonpath='{.metadata.name}' --ignore-not-found=true )
 	if [ ! ${ns} ]; then
 		return 0
 	fi
